f127f5088e31cb026f179f709803edfbebb13cee
Camilla Smith <camilla.smith@kpmg.co.uk>
2019-03-25 15:36:31 +0000

    Completed this module

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,13 +10,13 @@ class Quiz extends Component {
         this.state = {quiz_position: 1 }
     }
     render () {
-        const isQuizEnd = ((this.state.quiz_postion -1) === quizData.quiz_questions.length)
+        const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
         return (
             <div>
               {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+              <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
             </div>
-        )
+            )
     }
    
 }
